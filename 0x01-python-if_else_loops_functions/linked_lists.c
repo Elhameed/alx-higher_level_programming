fffeea5aaa5485273f05fd57cf254c3ee3fe8541
Elhameed <teniolaajani2004@gmail.com>
2022-08-23 17:31:49 -0700

    new

--- 0x01-python-if_else_loops_functions/linked_lists.c
+++ 0x01-python-if_else_loops_functions/linked_lists.c
@@ -9,19 +9,19 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	const listint_t *current;
-	unsigned int n; /* number of nodes */
+        const listint_t *current;
+        unsigned int n; /* number of nodes */
 
-	current = h;
-	n = 0;
-	while (current != NULL)
-	{
-		printf("%i\n", current->n);
-		current = current->next;
-		n++;
-	}
+        current = h;
+        n = 0;
+        while (current != NULL)
+        {
+                printf("%i\n", current->n);
+                current = current->next;
+                n++;
+        }
 
-	return (n);
+        return (n);
 }
 
 /**
@@ -32,27 +32,28 @@ size_t print_listint(const listint_t *h)
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new;
-	listint_t *current;
+        listint_t *new;
+        listint_t *current;
 
-	current = *head;
+        current = *head;
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	new->next = NULL;
+        new = malloc(sizeof(listint_t));
+        if (new == NULL)
+                return (NULL);
 
-	if (*head == NULL)
-		*head = new;
-	else
-	{
-		while (current->next != NULL)
-			current = current->next;
-		current->next = new;
-	}
+        new->n = n;
+        new->next = NULL;
 
-	return (new);
+        if (*head == NULL)
+                *head = new;
+        else
+        {
+                while (current->next != NULL)
+                        current = current->next;
+                current->next = new;
+        }
+
+        return (new);
 }
 
 /**
@@ -60,15 +61,14 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
  * @head: pointer to list to be freed
  * Return: void
  */
-
 void free_listint(listint_t *head)
 {
-	listint_t *current;
+        listint_t *current;
 
-	while (head != NULL)
-	{
-		current = head;
-		head = head->next;
-		free(current);
-	}
+        while (head != NULL)
+        {
+                current = head;
+                head = head->next;
+                free(current);
+        }
 }
