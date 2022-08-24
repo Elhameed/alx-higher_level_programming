da63c074cb259ba5abb2d83ab427db970cc2e78d
Elhameed <teniolaajani2004@gmail.com>
2022-08-23 17:29:44 -0700

    new

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,45 +1,42 @@
-#include <stdlib.h>
-#include <string.h>
 #include "lists.h"
-/**
- * insert_node - inserts a node in a linked list
- * @head: double pointer to the head of the list
- * @number: value to be added to node
- * Return: the address of the new node, or NULL if it failed
- */
 
+/**
+ * insert_node - inserts a number into a sorted singly linked list.
+ * @head: list head
+ * @number: number to store in the new node
+ * Return: pointer to the new node
+ */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *current, *temp, *new;
+        listint_t *runner;
+        listint_t *new;
 
-	current = *head, temp = *head, new = NULL;
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-	{
-		return (NULL);
-	}
-	if (*head == NULL)
-	{
-		new->n = number;
-		new->next = NULL;
-		*head = new;
-		return (new);
-	}
-	while (current->next)
-	{
-		current = current->next;
-		if (current->n >= number)
-		{
-			new->n = number;
-			new->next = current;
-			temp->next = new;
-			return (new);
-		}
-		else
-		{
-			temp = temp->next;
-		}
-	}
-	new = add_nodeint_end(head, number);
-	return (new);
+        runner = *head;
+
+        new = malloc(sizeof(listint_t));
+        if (new == NULL)
+                return (NULL);
+        new->n = number;
+
+        if (*head == NULL || (*head)->n > number)
+        {
+                new->next = *head;
+                *head = new;
+                return (new);
+        }
+
+        while (runner->next != NULL)
+        {
+                if ((runner->next)->n >= number)
+                {
+                        new->next = runner->next;
+                        runner->next = new;
+                        return (new);
+                }
+                runner = runner->next;
+        }
+
+        new->next = NULL;
+        runner->next = new;
+        return (new);
 }
